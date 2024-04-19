ba5824172996dcc964d2a5edbf9f2a7dddf33a66
zp308519 <zp308519@student.polsl.pl>
2024-04-19 18:54:03 +0200

    Revert "new bug"
    This reverts commit 87328f24fc9f2735ac34d1116d6e2b4561d2f899.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -56,5 +56,5 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
-today is a good day
+
 }
